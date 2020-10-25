340739924e7bd7cfb4273b23a02f83f22d47ea36
noah <noahlog@naver.com>
2020-10-26 02:57:27 +0900

    Create simple calculator

--- src/index.js
+++ src/index.js
@@ -5,25 +5,110 @@
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
-  Object.entries(props || {}).forEach(([key, value]) => {
-    element[key.toLowerCase()] = value;
-  });
+  Object.entries(props || {})
+    .forEach(([key, value]) => {
+      element[key.toLowerCase()] = value;
+    });
 
-  children.flat().forEach((child) => {
-    if (child instanceof Node) {
-      element.appendChild(child);
-      return;
-    }
-    element.appendChild(document.createTextNode(child));
-  });
+  children.flat()
+    .forEach((child) => {
+      if (child instanceof Node) {
+        element.appendChild(child);
+        return;
+      }
+      element.appendChild(document.createTextNode(child));
+    });
 
   return element;
 }
 
-function render() {
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    }
+    return function pass(...args2) {
+      return curried.apply(this, [...args, ...args2]);
+    };
+  };
+}
+
+const operators = ['+', '-', '*', '/', '='];
+
+function calculate(x, f, y) {
+  return {
+    '+': x + y,
+    '-': x - y,
+    '*': x * y,
+    '/': x / y,
+    '=': x,
+  }[f];
+}
+
+const curriedCalculate = curry(calculate);
+
+const initialState = {
+  acc: 0,
+  cur: 0,
+  calculating: curriedCalculate,
+};
+
+function render({ acc, cur, calculating }) {
+  const handleClickNumber = (number) => {
+    render({
+      acc: acc * 10 + number,
+      cur: acc === 0 ? number : acc * 10 + number,
+      calculating,
+    });
+  };
+
+  const handleClickOperator = (operator) => {
+    const result = calculating(cur);
+
+    if (typeof result === 'number') {
+      render({
+        acc: 0,
+        cur: result,
+        calculating: operator === '='
+          ? curriedCalculate
+          : curriedCalculate(result, operator),
+      });
+      return;
+    }
+    render({
+      acc: 0,
+      cur,
+      calculating: calculating(cur, operator),
+    });
+  };
+
+  const handleClickReset = () => {
+    render(initialState);
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{cur}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+          <button type="button" onClick={() => handleClickNumber(number)}>
+            {number}
+          </button>
+        ))}
+      </p>
+      <p>
+        {operators.map((operator) => (
+          <button type="button" onClick={() => handleClickOperator(operator)}>
+            {operator}
+          </button>
+        ))}
+      </p>
+      <p>
+        <button type="button" onClick={handleClickReset}>
+          Clear
+        </button>
+      </p>
     </div>
   );
 
@@ -31,4 +116,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(initialState);
